6a9d63cc9e5cd616d61890f3e3a751cc618b95f7
natinha <naticamposs010607@gmail.com>
2025-04-10 09:40:05 -0300

    Welcome e Login

---
+++ src/pages/Login.jsx
@@ -0,0 +1,15 @@
+import Logo from "../img/logo.png";
+
+export default function Login() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <header className="">
+        <img src={Logo} alt=" " className="w-32 h-auto bg-blue-500" />
+      </header>
+      <div className="">
+ 
+      </div>
+    </div>
+  );
+}
+
---
+++ src/pages/Welcome.jsx
@@ -0,0 +1,17 @@
+export default function Welcome() {
+  return (
+    <>
+      <div className="bg-[url('/src/img/fundoWelcome.jpg')] bg-cover bg-center relative h-screen w-screen">
+        {/* Texto sobre o fundo */}
+        <p className="text-white text-4xl p-4 z-10">BibliON</p>
+        <img
+          src="/src/img/bookWelcome.jpg"
+          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-w-xs z-20"
+        />
+        <p>Bem-vindo(a) à Biblioteca Online!</p>
+        <button>Sou aluno/professor</button>
+        <button>Sou bibliotecário</button>
+      </div>
+    </>
+  );
+}
