0db15f2b5b356578169e8609737947a7b6198973
laraheloisa <130068472+laraheloisa@users.noreply.github.com>
2025-04-19 10:55:12 -0300

    .

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,44 +10,7 @@ export default function Login() {
         />
       </header>
 
-      <div className="absolute top-10 left-1/2 transform -translate-x-1/2 z-10">
-        <form className="bg-white p-8 rounded-2xl shadow-md w-100 h-120 flex flex-col gap-4 mt-20">
-          <h2 className="text-3xl  text-center text-blue-700 mb-2">Login</h2>
-
-          <label className="flex flex-col text-gray-700 font-semibold">
-            E-mail
-            <input
-              type="text"
-              className="mt-1 p-4 border border-gray-300 bg-gray-300 rounded-md"
-              placeholder="Digite seu e-mail"
-            />
-          </label>
-
-          <label className="flex flex-col text-gray-700 font-semibold">
-            Senha
-            <input
-              type="number"
-              className="mt-1 p-4 border border-gray-300 bg-gray-300 rounded-md"
-              placeholder="Digite sua senha"
-            />
-          </label>
-
-          <div className="flex flex-col items-center">
-            <p className="text-base text-center text-gray-600">
-              Não tem uma conta?{" "}
-            </p>
-            <a href="#" className="text-blue-700 font-bold hover:underline">
-              Cadastre-se
-            </a>
-          </div>
-
-          <button className="bg-blue-950 text-black px-4 py-2 rounded hover:bg-blue-600">
-            oi
-          </button>
-
-        </form>
-      </div>
-
+    
     </div>
   );
 }
