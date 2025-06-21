f12ddde2f05045f6d9aa6e3e3bd3aa237fa2afcb
natinha <naticamposs010607@gmail.com>
2025-06-21 16:17:27 -0300

    Listagem de clientes

--- src/pages/Clientes.jsx
+++ src/pages/Clientes.jsx
@@ -1,12 +1,14 @@
 //import { Pencil } from "@tabler/icons-react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Menu from "../components/Menu";
 import { BarraPesquisa } from "../components/BarraPesquisa";
 import { useNavigate } from "react-router-dom";
 import Layout from "../components/Layout";
 import Botao from "../components/Botao";
+
 export default function Clientes() {
   const [filtro, setFiltro] = useState("");
+  const [locatarios, setLocatarios] = useState([])
 
   const navigate = useNavigate();
 
@@ -18,41 +20,25 @@ export default function Clientes() {
     navigate("/cliente");
   };
 
-  const listaClientes = [
-    {
-      nome: "Natália Campos Soares",
-      cargo: "Aluno",
-      curso: "Técnico em Informática",
-    },
-    {
-      nome: "Fernando Manso",
-      cargo: "Professor",
-      curso: "Técnico Informática",
-    },
-    {
-      nome: "Amanda Soares",
-      cargo: "Professor",
-      curso: "Técnico Informática",
-    },
-    {
-      nome: "Lara Deitos",
-      cargo: "Professor",
-      curso: "Técnico Informática",
-    },
-    {
-      nome: "Fernando Manso",
-      cargo: "Professor",
-      curso: "Técnico Informática",
-    },
-    {
-      nome: "Fernando Manso",
-      cargo: "Professor",
-      curso: "Técnico Informática",
-    },
-    { nome: "Sara Guaiume", cargo: "Bibliotecário", curso: "" },
-  ];
+  useEffect(() => {
+    async function buscarLocatarios() {
+      try {
+        const resposta = await fetch("http://localhost:3000/listarLocatarios"); 
+        if (!resposta.ok) {
+          throw new Error("Erro ao buscar locatários");
+        }
+        const data = await resposta.json();
+console.log("Dados recebidos:", data);
+        setLocatarios(data);
+      } catch (error) {
+        console.error("Erro ao buscar locatários:", error);
+      }
+    }
 
-  const clientesFiltrados = listaClientes.filter((cliente) =>
+    buscarLocatarios();
+  }, []);
+
+  const clientesFiltrados = locatarios.filter((cliente) =>
     cliente.nome.toLowerCase().includes(filtro.toLowerCase())
   );
 
@@ -60,7 +46,7 @@ export default function Clientes() {
     <Layout>
       <div className="flex flex-col w-full h-full">
         <div className="shrink-0">
-          <BarraPesquisa />
+          <BarraPesquisa filtro={filtro} setFiltro={setFiltro}/>
         </div>
         <div
           className="flex-1 overflow-y-auto px-4 mt-4 flex flex-col gap-2"
