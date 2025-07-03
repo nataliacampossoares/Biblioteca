965376c0605b59172d8355f617cfa46ad5ba564d
natinha <naticamposs010607@gmail.com>
2025-07-03 07:43:04 -0300

    Update PaginaClientes.jsx

--- src/pages/PaginaClientes.jsx
+++ src/pages/PaginaClientes.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Layout from "../components/LayoutsemMenu";
 import { BarraPesquisa } from "../components/BarraPesquisa";
-import Botao from "../components/Botao";
 
 export default function LivrosBibliotecario() {
   const [categoria, setCategoria] = useState("");
